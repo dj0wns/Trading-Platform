dfbef73f77a75934eb35c1d4d454fe08e03ed6b3
dj0wns <derekjones@asu.edu>
2015-05-05 14:46:28 -0700

    Add functions to OrderController

--- Trading Platform/OrderController.h
+++ Trading Platform/OrderController.h
@@ -6,6 +6,8 @@
 
 #include "stdafx.h"
 #include "Order.h"
+#include "Globals.h"
+#include "threadController.h"
 
 
 class OrderController
@@ -13,14 +15,26 @@ class OrderController
 public:
 
 	OrderController();
+	//adds and order to the queue
+	void queueOrder(funct, int, float, float, float);
+	//executes an order in the queue at the index
+	void executeOrder(int); //TODO
+
+	std::vector<Order*> getCompleted();
+	std::vector<Order*> getInProgress();
+	std::vector<Order*> getQueue();
 
 protected:
 private:
+
+	//holding for completed orders, to be cleaned as they are written to the log file
+	std::vector<Order*> completed;
+
 	//vector of orders in progress (as pointers to lessen run time)
 	std::vector<Order*> inProgress;
 
 	//vector of orders 
-	std::vector<Order*> queued;
+	std::vector<Order*> queue;
 
 };
 
